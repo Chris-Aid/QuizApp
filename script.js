0b3c70a60f314c3ab2c854f06094e8f6795c1c86
Christian Aidelsburger <c.aidelsburger@gmx.de>
2022-02-15 20:27:43 +0100

    create design + question arrays

---
+++ script.js
@@ -0,0 +1,60 @@
+let questions = [
+{
+    "question": "Wer hat JavaScript erfunden?",
+    "answer_1": "Slipknot",
+    "answer_2": "dein Mudda",
+    "answer_3": "Brendan Eich",
+    "answer_4": "Michael Ballack",
+    "right_answer": 3
+},
+{
+    "question": "In welchem ​​Jahr ist die Titanic am 15. April auf ihrer Jungfernfahrt von Southampton im Atlantik gesunken?",
+    "answer_1": "1912",
+    "answer_2": "2012",
+    "answer_3": "1966",
+    "answer_4": "1945",
+    "right_answer": 1
+},
+{
+    "question": "Was ist die Lebensdauer einer Libelle?",
+    "answer_1": "3 Jahre",
+    "answer_2": "2 Jahre",
+    "answer_3": "1 Jahre",
+    "answer_4": "2 Monate",
+    "right_answer": 4
+},
+{
+    "question": "In welchem ​​Jahr wurde The Godfather zum ersten Mal veröffentlicht?",
+    "answer_1": "1968",
+    "answer_2": "1972",
+    "answer_3": "1978",
+    "answer_4": "1982",
+    "right_answer": 2
+},
+{
+    "question": "Welche amerikanische Popgruppe der 1960er Jahre hat den Surfin Sound kreiert?",
+    "answer_1": "Slipknot",
+    "answer_2": "Beach Boys",
+    "answer_3": "Haze Shuttle",
+    "answer_4": "Metallica",
+    "right_answer": 2
+},
+{
+    "question": " Wer hat einen Hammer und eine Feder auf den Mond fallen lassen, um zu demonstrieren, dass sie ohne Luft gleich schnell fallen?",
+    "answer_1": "David R. Scott",
+    "answer_2": "Rupert Grey",
+    "answer_3": "Edinson Ferger",
+    "answer_4": "Petricia Fade",
+    "right_answer": 1
+},
+{
+    "question": "Welcher Künstler schuf 1962 'Campbell's Soup Cans'?",
+    "answer_1": "Andy Warhol",
+    "answer_2": "Michael Scott",
+    "answer_3": "Henry Moore",
+    "answer_4": "Peter Griffin",
+    "right_answer": 1
+}
+
+
+];
